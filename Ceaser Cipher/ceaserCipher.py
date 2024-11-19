e8d8e045a300d99e0f038c683ead1765fd591b25
syncattacker <cybereyerahul@gmail.com>
2024-11-19 23:24:33 +0530

    Encryption and Decryption Functionality Added.

--- Ceaser Cipher/ceaserCipher.py
+++ Ceaser Cipher/ceaserCipher.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Import the necessary libraries
-import time
+from time import sleep
 from art import text2art
 import os
 
@@ -25,11 +25,10 @@ def userChoice() -> int:
     '''
     Prints a menu to the user to select operations needed to be performed.
     '''
-    print("1. Encrypt")
+    print("\n\n1. Encrypt")
     print("2. Decrypt")
     print("3. Brute-Force")
-    print("4. Exit")
-
+    print("4. Exit\n")
     while True:
         try:
             choice = int(input("Enter the task to be performed : "))
@@ -42,6 +41,7 @@ def userChoice() -> int:
         else:
             return choice
 
+# Function to encrypt the user message.
 def encrypt(plainText : str, encryptionKey : int) -> str:
     '''
     Encrypt the plain text and return the encrypted text using the user key.
@@ -54,6 +54,19 @@ def encrypt(plainText : str, encryptionKey : int) -> str:
             cipherText += chr((((ord(character) - ord('A')) + encryptionKey) % 26) + ord('A'))
     return cipherText
 
+# Function to decrypt the user message.
+def decrypt(cipherText : str, decryptionKey : int) -> str:
+    plainText = ''
+    for character in cipherText:
+        if character.islower():
+            plainText += chr((((ord(character) - ord('a')) - decryptionKey) % 26) + ord('a'))
+        elif character.isupper():
+            plainText += chr((((ord(character) - ord('A')) - decryptionKey) % 26) + ord('A'))
+    return plainText
+
+# Function to brute force the key based on plain text and cipher text.
+def bruteForce(plainText : str, cipherText : str) -> int:
+    pass
 
 # Function to check whether the message is only alphabets.
 def validateMessage(message : str) -> bool:
@@ -67,5 +80,57 @@ def validateKey(userKey : int) -> bool:
     '''
     Validating the key entered by the user is correct or not.
     '''
-    pass
+    return False if ((userKey < 0) or (userKey > 25)) else True
 
+# Handle the user message input.
+def handleUserInput(inputType : str, keyType : str) -> list:
+    '''
+    Handles the user input for encryption of data provided by user.
+    '''
+    list = []
+    while True:
+        try:
+            message = input(f"Enter Your {inputType} Text : ")
+            if validateMessage(message):
+                list.append(message)
+                while True:
+                    try:
+                        key = int(input(f"Enter The {keyType} Key : "))
+                        if validateKey(key):
+                            list.append(key)
+                            break
+                    except ValueError:
+                        print("[-] Invalid Key.")
+                        continue
+                    else:
+                        print("[-] Invalid Key! Key domain 0 to 25")
+                        continue
+            else:
+                print("[-] Invalid message, make sure it's all alphabets.")
+                continue
+        except Exception as error:
+            print(f"Some unexpected error occured {error}")
+        return list
+
+# Execution entry point of code.
+def entryPoint() -> None:
+    '''
+    Main execution point of all the functions.
+    '''
+    clearTerminal()
+    sleep(0.6)
+    banner()
+    while True:
+        user = userChoice()
+        if user == 1:
+            message, key = handleUserInput('Plain Text', 'Encryption Key')
+            cipherText = encrypt(message, key)
+            print(f"Your Cipher Text is : {cipherText}")
+        elif  user == 2:
+            message, key = handleUserInput('Cipher Text', 'Decryption Key')
+            plainText = decrypt(message, key)
+            print(f"Your Plain Text is : {plainText}")
+        elif user == 4:
+            exit()
+
+entryPoint()
